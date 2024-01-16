eda83f66af6a46715ca4ba2bc39756172166285f
Louis Boeckmans <louis.boeckmans@outlook.com>
2024-01-16 16:40:24 +0100

    main

---
+++ Designer.DB/Tables/Folders.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Folders]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[Name] NVARCHAR(150) NOT NULL,
+	[CreationDate] DATETIME NOT NULL,
+	[LastUpdateDate] DATETIME NOT NULL,
+	[ProjectId] INT NOT NULL,
+	[ParentFolderId] INT,
+	CONSTRAINT [PK_Folders] PRIMARY KEY CLUSTERED ([Id]),
+	CONSTRAINT [FK_Folders_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Projects]([Id])
+)
