fe88e135003b7d9d313cd7ebc9d35216ab42ebff
Louis Boeckmans <louis.boeckmans@outlook.com>
2024-01-24 16:37:51 +0100

    main

--- Designer.DB/Scripts/Script.PostDeployment.sql
+++ Designer.DB/Scripts/Script.PostDeployment.sql
@@ -11,7 +11,7 @@ Post-Deployment Script Template
 */
 
 INSERT INTO [dbo].[Users] ([Firstname], [Lastname], [Email], [Username], [Password], [UserRole], [ActiveProjectId])
-VALUES ('Louis', 'Boeckmans', 'louis.boeckmans@outlook.com', 'LouisB', 'designer', 'Admin', 0);
+VALUES ('Louis', 'Boeckmans', 'louis.boeckmans@outlook.com', 'LouisB', 'designer', 'Admin', 1);
 
 INSERT INTO [dbo].[Projects] ([Name], [CreationDate], [LastUpdateDate], [UserId])
 VALUES ('Template', GETDATE(), GETDATE(), 1),
@@ -19,13 +19,23 @@ VALUES ('Template', GETDATE(), GETDATE(), 1),
 
 
 INSERT INTO [dbo].[Folders] ([Name], [CreationDate], [LastUpdateDate], [ProjectId], [ParentFolderId], [IsEditable], [IsSelected], [IsExpanded])
-VALUES ('Template', GETDATE(), GETDATE(), 1, 0, 'false', 'false', 'false'),
-       ('Template 0.1', GETDATE(), GETDATE(), 1, 1, 'false', 'false', 'false'),
-       ('Template 0.2', GETDATE(), GETDATE(), 1, 1, 'false', 'false', 'false'),
-       ('Template 0.1.1', GETDATE(), GETDATE(), 1, 2, 'false', 'false', 'false'),
-       ('Template 0.2.1', GETDATE(), GETDATE(), 1, 3, 'false', 'false', 'false'),
-       ('Model', GETDATE(), GETDATE(), 2, 0, 'false', 'false', 'false'),
-       ('Model 0.1', GETDATE(), GETDATE(), 2, 6, 'false', 'false', 'false'),
-       ('Model 0.2', GETDATE(), GETDATE(), 2, 6, 'false', 'false', 'false'),
-       ('Model 0.1.1', GETDATE(), GETDATE(), 2, 7, 'false', 'false', 'false'),
-       ('Model 0.2.1', GETDATE(), GETDATE(), 2, 8, 'false', 'false', 'false');
+VALUES ('Template', GETDATE(), GETDATE(), 1, 0, 'false', 'false', 'false'), /* 1 */
+       ('Folder 1', GETDATE(), GETDATE(), 1, 1, 'false', 'false', 'false'), /* 2 */
+       ('Folder 2', GETDATE(), GETDATE(), 1, 1, 'false', 'false', 'false'), /* 3 */
+       ('Folder 1.1', GETDATE(), GETDATE(), 1, 2, 'false', 'false', 'false'), /* 4 */
+       ('Folder 2.1', GETDATE(), GETDATE(), 1, 3, 'false', 'false', 'false'), /* 5 */
+       ('Models', GETDATE(), GETDATE(), 2, 0, 'false', 'false', 'false'), /* 6 */
+       ('Folder 1', GETDATE(), GETDATE(), 2, 6, 'false', 'false', 'false'), /* 7 */
+       ('Folder 2', GETDATE(), GETDATE(), 2, 6, 'false', 'false', 'false'), /* 8 */
+       ('Folder 1.1', GETDATE(), GETDATE(), 2, 7, 'false', 'false', 'false'), /* 9 */
+       ('Folder 2.1', GETDATE(), GETDATE(), 2, 8, 'false', 'false', 'false'); /* 10 */
+
+INSERT INTO [dbo].[Components] ([Name], [CreationDate], [LastUpdateDate], [ProjectId], [ParentFolderId], [IsEditable], [IsSelected], [IsExpanded])
+VALUES ('Component 1', GETDATE(), GETDATE(), 1, 2, 'false', 'false', 'false'), /* 1 */
+       ('Component 2', GETDATE(), GETDATE(), 1, 3, 'false', 'false', 'false'), /* 2 */
+       ('Component 1.1', GETDATE(), GETDATE(), 1, 4, 'false', 'false', 'false'), /* 3 */
+       ('Component 2.1', GETDATE(), GETDATE(), 1, 5, 'false', 'false', 'false'), /* 4 */
+       ('Component 1', GETDATE(), GETDATE(), 2, 7, 'false', 'false', 'false'), /* 5 */
+       ('Component 2', GETDATE(), GETDATE(), 2, 8, 'false', 'false', 'false'), /* 6 */
+       ('Component 1.1', GETDATE(), GETDATE(), 2, 9, 'false', 'false', 'false'), /* 7 */
+       ('Component 2.1', GETDATE(), GETDATE(), 2, 10, 'false', 'false', 'false'); /* 8 */
---
+++ Designer.DB/Tables/Components.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Components]
+(
+	[Id] INT NOT NULL IDENTITY(1,1),
+	[Name] NVARCHAR(150) NOT NULL,
+	[CreationDate] DATETIME NOT NULL,
+	[LastUpdateDate] DATETIME NOT NULL,
+	[ProjectId] INT NOT NULL,
+	[ParentFolderId] INT NOT NULL,
+	[IsEditable] BIT NOT NULL,
+	[IsSelected] BIT NOT NULL,
+	[IsExpanded] BIT NOT NULL,
+	CONSTRAINT [PK_Components] PRIMARY KEY CLUSTERED ([Id]),
+	CONSTRAINT [FK_Components_Project] FOREIGN KEY ([ProjectId]) REFERENCES [dbo].[Projects]([Id]),
+	CONSTRAINT [FK_Components_Folder] FOREIGN KEY ([ParentFolderId]) REFERENCES [dbo].[Folders]([Id])
+)
